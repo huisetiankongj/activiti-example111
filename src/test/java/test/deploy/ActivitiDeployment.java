84a0e5dd2443dd40889962c00059f6f7ba96fb40
huisetiankongj <421310742@qq.com>
2017-01-11 17:35:35 +0800

    第一提交

---
+++ src/test/java/test/deploy/ActivitiDeployment.java
@@ -0,0 +1,19 @@
+package test.deploy;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.junit.Test;
+
+public class ActivitiDeployment {
+
+	@Test
+	public void zipDeploy(){
+		
+		//ªÒ»°≈‰÷√
+		ProcessEngineConfiguration config = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("test/activiti.cfg.xml");
+		ProcessEngine processEngine = config.buildProcessEngine();
+		
+	}
+	
+	
+}
